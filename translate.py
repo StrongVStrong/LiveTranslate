1a28788bf736c1bf88e4d0e0c36b26318cdd0f85
StrongVStrong <bha2095@gmail.com>
2024-12-11 21:46:18 -0500

    Reuploaded after fixes

--- translate.py
+++ translate.py
@@ -4,7 +4,6 @@ import tkinter as tk
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
